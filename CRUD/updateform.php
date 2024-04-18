5438e2cb1094156b2359f8ed9052ad556acfd9fc
Sakai-Kaede <kana1024k@gmail.com>
2024-04-18 22:43:27 +0900

    Update processing in list.php.

--- CRUD/updateform.php
+++ CRUD/updateform.php
@@ -7,12 +7,23 @@
     <title>PHPテスト</title>
   </head>
   <body>
+    <hr>
+    更新画面
+    <hr>
+    [ <a href="list.php">戻る</a>]
+    <br>
+
     <?php
       require_once("MYDB.php");
       $pdo = db_connect();
 
-      $id = 1;
-      $_SESSION['id'] = $id;
+      if(isset($_GET['id']) && $_GET['id'] > 0){
+        $id = $_GET['id'];
+        $_SESSION['id'] = $id;
+      } else {
+        exit('パラメータが不正です');
+      }
+      
       try{
         $sql = "SELECT * FROM member WHERE id = :id";
         $stmh = $pdo->prepare($sql);
@@ -27,11 +38,18 @@
         print "更新データがありません<br>";
       } else {
         $row = $stmh->fetch(PDO::FETCH_ASSOC);
-      }
     ?>
 
     <form name="form1" method="post" action="list.php">
       番号：<?=htmlspecialchars($row['id'])?><br>
       氏：<input type="text" name="last_name" value="<?=htmlspecialchars($row['last_name'],ENT_QUOTES)?>"><br>
+      名：<input type="text" name="first_name" value="<?=htmlspecialchars($row['first_name'],ENT_QUOTES)?>"><br>
+      <input type="hidden" name="action" value="update">
+      <input type="submit" value="更 新">
     </form>
+
+    <?php
+      }
+    ?>
   </body>
+</html>
